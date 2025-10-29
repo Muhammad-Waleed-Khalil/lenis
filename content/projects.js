2d1b3a05923dd1845aab1c9ace9f7355d2b766cc
mwaleedkhalil <mwaleedkhalil@gmail.com>
2025-10-29 12:20:17 +0500

    Initial commit for my lenis website

---
+++ content/projects.js
@@ -0,0 +1,57 @@
+export const projects = [
+  // {
+  //   title: 'Deso',
+  //   source: 'Studio Freight',
+  //   href: 'https://deso.com',
+  // },
+  // {
+  //   title: 'Sculpting Harmony',
+  //   source: 'Resn',
+  //   href: 'https://gehry.getty.edu/',
+  // },
+  // {
+  //   title: 'Superpower',
+  //   source: '',
+  //   href: 'https://superpower.com/',
+  // },
+  // {
+  //   title: 'Daylight Computer',
+  //   source: 'Basement Studio',
+  //   href: 'https://daylightcomputer.com/',
+  // },
+  // {
+  //   title: 'Lifeworld by Olafur Eliasson',
+  //   source: '',
+  //   href: 'https://lifeworld.wetransfer.com/',
+  // },
+  {
+    title: 'Ibicash',
+    source: 'darkroom.engineering',
+    href: 'https://ibi.cash',
+  },
+  {
+    title: 'Grand Theft Auto VI',
+    source: 'Rockstar Games',
+    href: 'https://www.rockstargames.com/VI',
+  },
+  {
+    title: 'Sculpting Harmony',
+    source: 'Resn',
+    href: 'https://gehry.getty.edu/',
+  },
+  {
+    title: 'Microsoft Design',
+    source: 'Microsoft',
+    href: 'https://microsoft.design/',
+  },
+  {
+    title: 'Deso',
+    source: 'Studio Freight',
+    href: 'https://deso.com',
+  },
+  {
+    title: 'Shopify Supply',
+    source: 'Shopify',
+    href: 'https://shopify.supply/',
+  },
+]
